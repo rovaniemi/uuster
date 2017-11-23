ca79adbc46950b0b1d8b599301bdc28350d4ea2e
Mauri Karlin <karlin@cs.helsinki.fi>
2017-11-23 13:41:15 +0200

    skeleton

---
+++ src/main/java/uuster/Uuster.java
@@ -0,0 +1,13 @@
+package uuster;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uuster {
+    public static void main(String[] args) {
+        SpringApplication.run(Uuster.class, args);
+    }
+}
