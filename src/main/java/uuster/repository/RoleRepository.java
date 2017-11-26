99ebbda79898a8a72586b2d853535133b08f9183
Mauri Karlin <karlin@cs.helsinki.fi>
2017-11-26 18:37:14 +0200

    new user get journalist role

--- src/main/java/uuster/repository/RoleRepository.java
+++ src/main/java/uuster/repository/RoleRepository.java
@@ -2,7 +2,8 @@ package uuster.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import uuster.domain.Role;
+import java.util.List;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String string);
+    List<Role> findByName(String string);
 }
