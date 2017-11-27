16b514f7026833ac85ee726672b84e6e8a42c4d5
Mauri Karlin <karlin@cs.helsinki.fi>
2017-11-27 16:34:59 +0200

    heroku fix

--- src/main/java/uuster/Uuster.java
+++ src/main/java/uuster/Uuster.java
@@ -15,7 +15,6 @@ public class Uuster {
     public void started() {
         TimeZone.setDefault(TimeZone.getTimeZone("Europe/Helsinki"));
     }
-
     public static void main(String[] args) {
         SpringApplication.run(Uuster.class, args);
     }
